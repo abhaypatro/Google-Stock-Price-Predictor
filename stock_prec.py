4f48dcdf4d7f85a985f86feb589edc94ccaa1b4d
abhaypatro <abhaycharanpatro@gmail.com>
2019-12-09 15:29:41 +0530

    Updated to include graph
    Used matplotlib to create graph for predicting the price of stocks.

--- stock_prec.py
+++ stock_prec.py
@@ -4,6 +4,11 @@ import quandl
 import math
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df=quandl.get('WIKI/GOOGL')
 df=df[[ 'Adj. Open' ,  'Adj. High' ,   'Adj. Low' ,  'Adj. Close' ,  'Adj. Volume']]
@@ -18,13 +23,15 @@ df.fillna(-99999, inplace=True)
 forecast_out=int(math.ceil(0.01*len(df)))
 
 df['label']=df[forecast_column].shift(-forecast_out)
+
+X = np.array(df.drop(['label'],1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+
 df.dropna(inplace=True)
-
-X=np.array(df.drop(['label'],1))
 y=np.array(df['label'])
-
-X=preprocessing.scale(X)
-
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
@@ -32,10 +39,28 @@ clf=LinearRegression()
 clf.fit(X_train,y_train)
 accuracy=clf.score(X_test,y_test)
 
+forecast_set=clf.predict(X_lately)
+
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 150)
 np.set_printoptions(threshold=np.inf)
-print(accuracy)
+print(forecast_set , accuracy , forecast_out )
+df['Forecast']=np.nan
 
+last_date=df.iloc[-1].name
+last_unix=last_date.timestamp()
+one_day=86400
+next_unix=last_unix + one_day
 
+for i in forecast_set:
+    next_date=datetime.datetime.fromtimestamp(next_unix)
+    next_unix+=one_day
+    df.loc[next_date]=[np.nan for _ in range(len(df.columns)-1)]+ [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
